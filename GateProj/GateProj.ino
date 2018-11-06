ea856c2f25dfee29d861941acfdc994942fb7a2c
Kash Admin <krmunir@gmail.com>
2018-11-06 16:00:49 +1030

    Add project files.

---
+++ GateProj/GateProj.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		GateProj.ino
+ Created:	11/6/2018 4:00:42 PM
+ Author:	kmunir
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
