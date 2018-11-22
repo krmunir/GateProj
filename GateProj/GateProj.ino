da675d1fd99029da40d407c626dcfb72ce81faf1
Kash Admin <krmunir@gmail.com>
2018-11-22 15:46:01 +1030

    added more Blynk functionality

--- GateProj/GateProj.ino
+++ GateProj/GateProj.ino
@@ -29,7 +29,7 @@
 char auth[] = "YourAuthToken";
 char ssid[] = "YourNetworkName";
 char pass[] = "YourPassword";
-
+BlynkTimer timer; //used to periodically push data to Blynk app
 
 /********** Pin Assignments **********/
 //Outputs
@@ -76,6 +76,11 @@ openSensor swingGateSouthStatus(SWING_GATE_South_PIN);
 led swingGateSouthLED(RGB_LED_South_R_PIN, RGB_LED_South_G_PIN, RGB_LED_South_B_PIN);
 
 
+/********** Main Program Forward Declarations **********/
+void pushBlynkDataToApp();
+
+
+/********** Main Program Code **********/
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
@@ -91,6 +96,7 @@ void setup()
 
 	//start Blynk
 	Blynk.begin(auth, ssid, pass);
+	timer.setInterval(250L, pushBlynkDataToApp);
 
 	//for blinking led
 	pinMode(D1, OUTPUT);
@@ -102,6 +108,7 @@ void loop()
 {
 	//run Blynk
 	Blynk.run();
+	timer.run(); //periodically pushes Blynk data to app
 	
 	//blink LED to indicate program running
 	digitalWrite(D1, HIGH);
@@ -109,5 +116,91 @@ void loop()
 	digitalWrite(D1, LOW);
 	delay(500);
 
-	slidingGateDistanceSensor.checkIfGateMoving();
+	slidingGateDistanceSensor.checkIfGateMoving(); //updates isOpening and isClosing status
 }
+
+/********** Write Data to app **********/
+void pushBlynkDataToApp() //this fn is called periodically based on interval set for 'Blynktimer timer' 
+{
+	//virtual pin 0 - Pushbutton 'Fully Open'
+
+	//virtual pin 1 - Pushbutton 'Half Open'
+
+	//virtual pin 2 - Pushbutton 'Fully Close'
+
+	//virtual pin 3 - LED 'isOpen'
+
+	//virtual pin 4 - LED 'isClosed'
+
+	//virtual pin 5 - LED 'isHalfOpen'
+
+	//virtual pin 6 - Styled button 'Status'
+
+	//virtual pin 7 - Bar chart 'Gate Position'
+
+	//virtual pin 8 - Terminal Input and Output
+
+	
+
+
+}
+
+
+
+/********** Read Data from app **********/
+BLYNK_WRITE(V0) {
+	//virtual pin 0 - Pushbutton 'Fully Open'
+
+
+}
+
+BLYNK_WRITE(V1) {
+	//virtual pin 1 - Pushbutton 'Half Open'
+
+
+}
+
+BLYNK_WRITE(V2) {
+	//virtual pin 2 - Pushbutton 'Fully Close'
+
+
+}
+
+BLYNK_WRITE(V3) {
+	//virtual pin 3 - LED 'isOpen'
+
+
+}
+
+BLYNK_WRITE(V4) {
+	//virtual pin 4 - LED 'isClosed'
+
+
+}
+
+BLYNK_WRITE(V5) {
+	//virtual pin 5 - LED 'isHalfOpen'
+
+
+}
+
+BLYNK_WRITE(V6) {
+	//virtual pin 6 - Styled button 'Status'
+
+
+}
+
+BLYNK_WRITE(V7) {
+	//virtual pin 7 - Bar chart 'Gate Position'
+
+
+}
+
+BLYNK_WRITE(V8) {
+	//virtual pin 8 - Terminal Input and Output
+
+
+}
+
+
+
